0dd86c7018b367469f56a352aa905e396bc627ef
ienaga <ienaga@tvon.jp>
2023-03-18 13:59:03 +0900

    #115 Sentry TypeError

--- src/javascript/external/ExternalLibrary.js
+++ src/javascript/external/ExternalLibrary.js
@@ -48,66 +48,68 @@ class ExternalLibrary
 
     /**
      * @param  {number} [frame=1]
-     * @return {HTMLImageElement}
+     * @return {Promise}
      * @method
      * @public
      */
     toImage (frame = 1)
     {
         if (!this._$instance) {
-            return new Image();
+            return Promise.resolve(new Image());
         }
 
         const bounds = this._$instance.getBounds(
             [1, 0, 0, 1, 0, 0], null, null, frame
         );
 
-        const width  = Math.abs(bounds.xMax - bounds.xMin);
-        const height = Math.abs(bounds.yMax - bounds.yMin);
+        const width  = Math.ceil(Math.abs(bounds.xMax - bounds.xMin));
+        const height = Math.ceil(Math.abs(bounds.yMax - bounds.yMin));
 
         const currentFrame = Util.$currentFrame;
 
         Util.$currentFrame = frame;
 
-        const canvas = this._$instance.draw(
-            Util.$getCanvas(),
-            Math.ceil(width),
-            Math.ceil(height),
+        return this
+            ._$instance
+            .draw(Util.$getCanvas(),
+                width,
+                height,
+                {
+                    "frame": frame,
+                    "matrix": [1, 0, 0, 1, 0, 0],
+                    "colorTransform": [1, 1, 1, 1, 0, 0, 0, 0],
+                    "blendMode": "normal",
+                    "filter": [],
+                    "loop": Util.$getDefaultLoopConfig()
+                },
+                null, frame
+            )
+            .then((canvas) =>
             {
-                "frame": frame,
-                "matrix": [1, 0, 0, 1, 0, 0],
-                "colorTransform": [1, 1, 1, 1, 0, 0, 0, 0],
-                "blendMode": "normal",
-                "filter": [],
-                "loop": Util.$getDefaultLoopConfig()
-            },
-            null,
-            frame
-        );
+                Util.$currentFrame = currentFrame;
 
-        Util.$currentFrame = currentFrame;
+                const image  = new Image();
+                image.width  = width;
+                image.height = height;
+                image.src    = canvas.toDataURL();
 
-        const image  = new Image();
-        image.width  = Math.ceil(width);
-        image.height = Math.ceil(height);
-        image.src    = canvas.toDataURL();
+                Util.$poolCanvas(canvas);
 
-        Util.$poolCanvas(canvas);
-
-        return image;
+                return image;
+            });
     }
 
     /**
      * @param  {object} point
      * @param  {string} path
-     * @return {boolean}
+     * @return {Promise}
      * @method
      * @public
      */
     addItemToDocument (point, path)
     {
         if (!point || !path) {
-            return false;
+            return Promise.resolve(false);
         }
 
         path = `${path}`;
@@ -126,22 +128,18 @@ class ExternalLibrary
                     `library-child-id-${instance.id}`
                 );
 
-            Util.$dragElement = {
-                "dataset": {
-                    "libraryId": instance.id
-                }
-            };
-
-            Util.$screen.drop({
-                "offsetX": point.x + Util.$offsetLeft,
-                "offsetY": point.y + Util.$offsetTop
-            });
-
-            Util.$dragElement = null;
-
-            return true;
+            return Util
+                .$screen
+                .drop({
+                    "offsetX": point.x + Util.$offsetLeft,
+                    "offsetY": point.y + Util.$offsetTop
+                })
+                .then(() =>
+                {
+                    return Promise.resolve(true);
+                });
         }
 
-        return false;
+        return Promise.resolve(false);
     }
 }
