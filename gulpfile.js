edfef400f272d2d18d59eceeaa95c034fcacbf94
ienaga <ienaga@tvon.jp>
2023-03-24 18:50:25 +0900

    #122 ショートカットキーの表示間違いを修正

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ const options = minimist(process.argv.slice(2), {
     "string": ["distPath", "version"],
     "default": {
         "prodBuild": false,
-        "version": "0.200.0",
+        "version": "0.201.0",
         "distPath": "."
     }
 });
