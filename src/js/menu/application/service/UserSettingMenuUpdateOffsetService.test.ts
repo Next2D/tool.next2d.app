7ebf94e65624c7819a3e8c351535336c4777d966
ienaga <ienaga@tvon.jp>
2023-10-16 14:28:30 +0900

    #147 テストケースを追加

---
+++ src/js/menu/application/service/UserSettingMenuUpdateOffsetService.test.ts
@@ -0,0 +1,32 @@
+import { $USER_MENU_NAME } from "../../../config/MenuConfig";
+import { $TOOL_USER_SETTING_ID } from "../../../config/ToolConfig";
+import { $registerMenu } from "../Menu";
+import { execute } from "./UserSettingMenuUpdateOffsetService";
+
+describe("UserSettingMenuUpdateOffsetServiceTest", () =>
+{
+    test("execute test", () =>
+    {
+        const menuMock = {
+            "name": $USER_MENU_NAME,
+            "offsetLeft": 0,
+            "offsetTop": 0
+        };
+
+        const menuDiv = document.createElement("div");
+        menuDiv.id = $USER_MENU_NAME;
+        document.body.appendChild(menuDiv);
+
+        const toolDiv = document.createElement("div");
+        toolDiv.id = $TOOL_USER_SETTING_ID;
+        document.body.appendChild(toolDiv);
+
+        $registerMenu(menuMock);
+        expect(menuMock.offsetLeft).toBe(0);
+        expect(menuMock.offsetTop).toBe(0);
+
+        execute();
+        expect(menuMock.offsetLeft).toBe(30);
+        expect(menuMock.offsetTop).toBe(80);
+    });
+});
