0e5059f2c7f95059139ad39802509e2ac916a6b6
ienaga <ienaga@tvon.jp>
2024-02-01 23:36:44 +0900

    #147 スクリプト操作を画面共有に対応(WIP)

--- src/js/history/application/timeline/TimelineTool/ScriptEditorDelete/usecase/ScriptEditorDeleteHistoryRedoUseCase.ts
+++ src/js/history/application/timeline/TimelineTool/ScriptEditorDelete/usecase/ScriptEditorDeleteHistoryRedoUseCase.ts
@@ -1,34 +1,58 @@
-import { $getCurrentWorkSpace } from "@/core/application/CoreUtil";
-import { $getLeftFrame, $getRightFrame } from "@/timeline/application/TimelineUtil";
+import type { MovieClip } from "@/core/domain/model/MovieClip";
+import type { InstanceImpl } from "@/interface/InstanceImpl";
+import { $getWorkSpace } from "@/core/application/CoreUtil";
 import { execute as timelineHeaderUpdateScriptElementService } from "@/timeline/application/TimelineHeader/service/TimelineHeaderUpdateScriptElementService";
 import { timelineHeader } from "@/timeline/domain/model/TimelineHeader";
+import { execute as scriptAreaReloadUseCase } from "@/controller/application/ScriptArea/usecase/ScriptAreaReloadUseCase";
+import {
+    $getLeftFrame,
+    $getRightFrame
+} from "@/timeline/application/TimelineUtil";
 
 /**
  * @description スクリプトを削除する
  *              Delete Script
  *
+ * @param  {number} work_space_id
+ * @param  {number} library_id
  * @param  {number} frame
  * @return {void}
  * @method
  * @public
  */
-export const execute = (frame: number): void =>
-{
-    // スクリプトを上書き
-    $getCurrentWorkSpace()
-        .scene
-        .deleteAction(frame);
+export const execute = (
+    work_space_id: number,
+    library_id: number,
+    frame: number
+): void => {
 
-    // 表示領域にあればアイコンを削除
-    if ($getLeftFrame() <= frame
-        && $getRightFrame() >= frame
-    ) {
-        const node = timelineHeader.elements[frame - $getLeftFrame()] as HTMLElement;
-        if (!node) {
-            return ;
+    const workSpace = $getWorkSpace(work_space_id);
+    if (!workSpace) {
+        return ;
+    }
+
+    const movieClip: InstanceImpl<MovieClip> | null = workSpace.getLibrary(library_id);
+    if (!movieClip) {
+        return ;
+    }
+
+    movieClip.deleteAction(frame);
+
+    if (workSpace.active && movieClip.active) {
+
+        // スクリプトエリアを再描画
+        scriptAreaReloadUseCase();
+
+        // 表示領域にあればアイコンを更新
+        if ($getLeftFrame() <= frame && $getRightFrame() >= frame) {
+
+            const node = timelineHeader.elements[frame - $getLeftFrame()] as HTMLElement;
+            if (!node) {
+                return ;
+            }
+
+            // ヘッダーの表示を更新
+            timelineHeaderUpdateScriptElementService(node, frame);
         }
-
-        // ヘッダーの表示を更新
-        timelineHeaderUpdateScriptElementService(node, frame);
     }
 };
--- src/js/history/application/timeline/TimelineTool/ScriptEditorUpdate/usecase/ScriptEditorUpdateHistoryRedoUseCase.ts
+++ src/js/history/application/timeline/TimelineTool/ScriptEditorUpdate/usecase/ScriptEditorUpdateHistoryRedoUseCase.ts
@@ -1,6 +1,9 @@
-import { $getCurrentWorkSpace } from "@/core/application/CoreUtil";
+import type { MovieClip } from "@/core/domain/model/MovieClip";
+import type { InstanceImpl } from "@/interface/InstanceImpl";
+import { $getWorkSpace } from "@/core/application/CoreUtil";
 import { execute as timelineHeaderUpdateScriptElementService } from "@/timeline/application/TimelineHeader/service/TimelineHeaderUpdateScriptElementService";
 import { timelineHeader } from "@/timeline/domain/model/TimelineHeader";
+import { execute as scriptAreaReloadUseCase } from "@/controller/application/ScriptArea/usecase/ScriptAreaReloadUseCase";
 import {
     $getLeftFrame,
     $getRightFrame
@@ -10,29 +13,48 @@ import {
  * @description 変更したスクリプトに上書きする
  *              Overwrite modified scripts
  *
+ * @param  {number} work_space_id
+ * @param  {number} library_id
  * @param  {number} frame
- * @param  {string} script
+ * @param  {string} after_script
  * @return {void}
  * @method
  * @public
  */
-export const execute = (frame: number, script: string): void =>
-{
-    // スクリプトを上書き
-    $getCurrentWorkSpace()
-        .scene
-        .setAction(frame, script);
+export const execute = (
+    work_space_id: number,
+    library_id: number,
+    frame: number,
+    after_script: string
+): void => {
 
-    // 表示領域にあればアイコンを更新
-    if ($getLeftFrame() <= frame
-        && $getRightFrame() >= frame
-    ) {
-        const node = timelineHeader.elements[frame - $getLeftFrame()] as HTMLElement;
-        if (!node) {
-            return ;
+    const workSpace = $getWorkSpace(work_space_id);
+    if (!workSpace) {
+        return ;
+    }
+
+    const movieClip: InstanceImpl<MovieClip> | null = workSpace.getLibrary(library_id);
+    if (!movieClip) {
+        return ;
+    }
+
+    movieClip.setAction(frame, after_script);
+
+    if (workSpace.active && movieClip.active) {
+
+        // スクリプトエリアを再描画
+        scriptAreaReloadUseCase();
+
+        // 表示領域にあればアイコンを更新
+        if ($getLeftFrame() <= frame && $getRightFrame() >= frame) {
+
+            const node = timelineHeader.elements[frame - $getLeftFrame()] as HTMLElement;
+            if (!node) {
+                return ;
+            }
+
+            // ヘッダーの表示を更新
+            timelineHeaderUpdateScriptElementService(node, frame);
         }
-
-        // ヘッダーの表示を更新
-        timelineHeaderUpdateScriptElementService(node, frame);
     }
 };
