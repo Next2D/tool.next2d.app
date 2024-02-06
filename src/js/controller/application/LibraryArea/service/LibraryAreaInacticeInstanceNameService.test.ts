decc638319cf870762fab92f273e9880adffb568
ienaga <ienaga@tvon.jp>
2024-02-06 21:47:40 +0900

    #147 テストケースを追加

---
+++ src/js/controller/application/LibraryArea/service/LibraryAreaInacticeInstanceNameService.test.ts
@@ -0,0 +1,21 @@
+import { execute } from "./LibraryAreaInacticeInstanceNameService";
+
+describe("LibraryAreaInacticeInstanceNameServiceTest", () =>
+{
+    test("execute test", () =>
+    {
+        const div = document.createElement("div");
+        div.contentEditable = "true";
+        div.style.borderBottom = "1px solid";
+
+        const eventMock = {
+            "target": div
+        };
+
+        expect(div.contentEditable).toBe("true");
+        expect(div.style.borderBottom).toBe("1px solid");
+        execute(eventMock);
+        expect(div.contentEditable).toBe("false");
+        expect(div.style.borderBottom).toBe("");
+    });
+});
