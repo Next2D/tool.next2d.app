5714d55f1cae9d9ef7033b3781e1e75018536729
ienaga <ienaga@tvon.jp>
2024-02-16 11:55:09 +0900

    #147 ライブラリの並び順を改修

--- src/js/controller/application/LibraryArea/service/LibraryAreaReOrderingService.ts
+++ src/js/controller/application/LibraryArea/service/LibraryAreaReOrderingService.ts
@@ -15,7 +15,10 @@ export const execute = (work_space: WorkSpace): void =>
     names.sort();
 
     // 名前順にインスタンスを配列に格納
-    const instances = [];
+    const instances   = [];
+    const instanceIds = [];
+
+    const folderMap = new Map();
     for (let idx: number = 0; idx < names.length; ++idx) {
 
         const name = names[idx];
@@ -25,20 +28,43 @@ export const execute = (work_space: WorkSpace): void =>
 
         const id = work_space.pathMap.get(name) as NonNullable<number>;
         const instance = work_space.getLibrary(id);
-        instances.push(instance);
+        if (!instance) {
+            continue;
+        }
+
+        // IDの配列に格納
+        instanceIds.push(id);
+
+        if (instance.folderId) {
+            // マップがなければ初期設定を追加
+            if (!folderMap.has(instance.folderId)) {
+                folderMap.set(instance.folderId, []);
+            }
+            folderMap.get(instance.folderId).push(instance);
+        } else {
+            instances.push(instance);
+        }
     }
 
     // 初期化
     work_space.libraries.clear();
     for (let idx = 0; idx < instances.length; ++idx) {
+
         const instance = instances[idx];
         work_space.libraries.set(instance.id, instance);
+
+        if (folderMap.has(instance.id)) {
+            const folderInInstances = folderMap.get(instance.id);
+            for (let idx = 0; idx < folderInInstances.length; ++idx) {
+                const instance = folderInInstances[idx];
+                work_space.libraries.set(instance.id, instance);
+            }
+        }
     }
 
     // マッピングも初期化
     work_space.pathMap.clear();
-    for (let idx = 0; idx < instances.length; ++idx) {
-        const instance = instances[idx];
-        work_space.pathMap.set(names[idx], instance.id);
+    for (let idx = 0; idx < instanceIds.length; ++idx) {
+        work_space.pathMap.set(names[idx], instanceIds[idx]);
     }
 };
