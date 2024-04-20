8ae0c91278dacd8027e904b2ffc11a8d099b864f
ienaga <ienaga@tvon.jp>
2024-04-20 11:42:05 +0900

    #147 Lint対応

--- src/js/history/application/controller/application/SoundArea/UpdateVolume/usecase/SoundAreaUpdateVolumeHistoryUndoUseCase.ts
+++ src/js/history/application/controller/application/SoundArea/UpdateVolume/usecase/SoundAreaUpdateVolumeHistoryUndoUseCase.ts
@@ -2,7 +2,6 @@ import type { InstanceImpl } from "@/interface/InstanceImpl";
 import type { MovieClip } from "@/core/domain/model/MovieClip";
 import { $getWorkSpace } from "@/core/application/CoreUtil";
 import { execute as soundAreaUpdateVolumeElementService } from "@/controller/application/SoundArea/service/SoundAreaUpdateVolumeElementService";
-import { $SOUND_AREA_SOUND_LIST_AREA_ID } from "@/config/PropertyConfig";
 
 /**
  * @description 個別の音声データの音量更新の履歴を登録を元に戻す
