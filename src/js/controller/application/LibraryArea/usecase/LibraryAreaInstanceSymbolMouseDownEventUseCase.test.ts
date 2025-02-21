12e42eeb0a2d4209b25404754d28b7bf1a56bb48
ienaga <ienaga@tvon.jp>
2025-02-22 06:57:40 +0900

    #147 テストケースを追加

---
+++ src/js/controller/application/LibraryArea/usecase/LibraryAreaInstanceSymbolMouseDownEventUseCase.test.ts
@@ -0,0 +1,67 @@
+import { execute } from "./LibraryAreaInstanceSymbolMouseDownEventUseCase";
+import { $useKeyboard } from "../../../../shortcut/ShortcutUtil";
+import {
+    $getNameSelectedLibraryId,
+    $getSymbolSelectedLibraryId,
+    $setNameSelectedLibraryId,
+    $setSymbolSelectedLibraryId
+} from "../LibraryAreaUtil";
+import { describe, expect, it, vi } from "vitest";
+
+describe("LibraryAreaInstanceSymbolMouseDownEventUseCase Test", () =>
+{
+    it("execute test case1", () =>
+    {
+        const div1 = document.createElement("div");
+        div1.dataset.libraryId = "1";
+
+        let preventDefault = false;
+        let stopPropagation = false;
+        $setNameSelectedLibraryId(-1);
+        $setSymbolSelectedLibraryId(-1);
+        const mockEvent = {
+            "currentTarget": div1,
+            "preventDefault": vi.fn(() => { preventDefault = true; }),
+            "stopPropagation": vi.fn(() => { stopPropagation = true; }),
+            "button": 0,
+            "altKey": false,
+            "metaKey": false,
+            "shiftKey": false,
+        } as unknown as PointerEvent;
+
+        expect($useKeyboard()).toBe(false);
+        expect($getNameSelectedLibraryId()).toBe(-1);
+        expect($getSymbolSelectedLibraryId()).toBe(-1);
+
+        execute(mockEvent);
+        expect($useKeyboard()).toBe(false);
+        expect($getNameSelectedLibraryId()).toBe(-1);
+        expect($getSymbolSelectedLibraryId()).toBe(1);
+
+        expect(preventDefault).toBe(false);
+        expect(stopPropagation).toBe(false);
+        execute(mockEvent);
+        expect($useKeyboard()).toBe(true);
+        expect(preventDefault).toBe(true);
+        expect(stopPropagation).toBe(true);
+        expect($getNameSelectedLibraryId()).toBe(-1);
+        expect($getSymbolSelectedLibraryId()).toBe(1);
+
+        const div2 = document.createElement("div");
+        div2.dataset.libraryId = "2";
+
+        const mockEvent2 = {
+            "currentTarget": div2,
+            "preventDefault": vi.fn(),
+            "stopPropagation": vi.fn(),
+            "button": 0,
+            "altKey": false,
+            "metaKey": false,
+            "shiftKey": false,
+        } as unknown as PointerEvent;
+
+        execute(mockEvent2);
+        expect($getNameSelectedLibraryId()).toBe(-1);
+        expect($getSymbolSelectedLibraryId()).toBe(2);
+    });
+});
