25aa9d9a3a0079d3d69c3d7ed513b39f48d11891
ienaga <ienaga@tvon.jp>
2025-08-31 13:07:37 +0900

    #151 ユニットテストを追加

---
+++ src/js/controller/application/TransformSetting/usecase/TransformSettingXFocusInEventUseCase.test.ts
@@ -0,0 +1,40 @@
+import { execute } from "./TransformSettingXFocusInEventUseCase";
+import { describe, expect, it, vi } from "vitest";
+import { $createWorkSpace, $getCurrentWorkSpace } from "../../../../core/application/CoreUtil";
+import type { WorkSpace } from "../../../../core/domain/model/WorkSpace";
+import { $useKeyboard, $updateKeyLock } from "../../../../shortcut/ShortcutUtil";
+import { transformSetting } from "../../../../controller/domain/model/TransformSetting";
+
+describe("TransformSettingXFocusInEventUseCase Test", () =>
+{
+    it("execute test case1", () =>
+    {
+        const workSpace: WorkSpace = $getCurrentWorkSpace() || $createWorkSpace();
+        const movieClip = workSpace.scene;
+        movieClip.selectedDepths.set(0, [0]);
+
+        const input = document.createElement("input");
+        input.value = "100";
+
+        let stopPropagation = false;
+        const mockEvent = {
+            currentTarget: input,
+            stopPropagation: vi.fn(() =>
+            {
+                stopPropagation = true;
+            })
+        } as unknown as FocusEvent;
+
+        // reset
+        transformSetting.clear();
+        $updateKeyLock(false);
+
+        expect(transformSetting.beforeX).toBe(0);
+        expect(stopPropagation).toBe(false);
+        expect($useKeyboard()).toBe(false);
+        execute(mockEvent);
+        expect(transformSetting.beforeX).toBe(100);
+        expect(stopPropagation).toBe(true);
+        expect($useKeyboard()).toBe(true);
+    });
+});
